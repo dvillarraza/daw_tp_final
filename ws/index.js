7d88c554fd90de7cadfd9e2624dab7f535492c55
dvilarraza <dvillarraza@hotmail.com>
2020-06-18 07:51:20 -0300

    Se resuelve Ejercicio 5.4

--- ws/index.js
+++ ws/index.js
@@ -5,19 +5,21 @@ var mysql = require('./mysql');
 app.use(express.json()); // para parsear application/json
 app.use(express.static('.')); // para servir archivos estaticos
 
-
-//EJ 12
 app.get('/devices', function(req, res, next) {
-    mysql.query('SELECT * FROM Devices', function(err, rta, field) {
-        if (err) {
-            res.send(err).status(400);
-            return;
-        }
-        res.send(rta).status(200);
-    });
-});
-app.get('/devices/:id', function(req, res, next) {
-    mysql.query('SELECT * FROM Devices WHERE id=?', [req.params.id], function(err, rta, field) {
+    var consulta = ""; //String para armar la consulta 
+    
+    //Segun el filtro se genera la consulta
+    if (req.query.filter == 0){ //Devuelve todos los dispositivos
+        consulta = "SELECT * FROM Devices";
+    }
+    if (req.query.filter == 1){ //Devuelve todas las lamparas
+        consulta = "SELECT * FROM Devices WHERE type=0";
+    }
+    if (req.query.filter == 2){ //Devuelve todas las persianas
+        consulta = "SELECT * FROM Devices WHERE type=1";
+    }
+
+    mysql.query(consulta, function(err, rta, field) {
         if (err) {
             res.send(err).status(400);
             return;
@@ -25,6 +27,7 @@ app.get('/devices/:id', function(req, res, next) {
         res.send(rta);
     });
 });
+
 app.post('/devices', function(req, res, next) {
 
     console.log(req.body);
